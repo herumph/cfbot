fc99f30ba3712c8fc3be48b2033650d3cb6cca8e
Scott Davis <sfdavis0@gmail.com>
2025-03-08 23:21:59 -0500

    query creds from db

--- post/login.py
+++ post/login.py
@@ -3,27 +3,31 @@
 import getpass
 from typing import Optional
 
-from atproto import Client, Session, SessionEvent
+from atproto import Client
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from db.create_db import init_db_session
+from db.models import Credentials
 
 
-def get_session(client: Client) -> Optional[str]:
+def get_session(client: Client, db_session: Session, login_type: str | None = None) -> Optional[str]:
     """Get session text files if they exist, otherwise prompt for username and
     password.
 
     Returns:
         session text information if it exists
     """
-    try:
-        with open("session.txt") as f:
-            return f.read()
-    except FileNotFoundError:
+    query = select(Credentials).filter(Credentials.type == login_type)
+    credentials = db_session.execute(query).first()
+    
+    if not credentials:
         user = getpass.getpass("user:")
         password = getpass.getpass("password:")
         client.login(user, password)
         session_string = client.export_session_string()
 
         save_session(session_string)
-        return session_string
+    return session_string
 
 
 def save_session(session_string: str):
@@ -36,27 +40,20 @@ def save_session(session_string: str):
         f.write(session_string)
 
 
-def on_session_change(event: SessionEvent, session: Session):
-    """Export session if there is a change.
-
-    Args:
-        event (SessionEvent): session trigger event
-        session (Sessions): bluesky session
-    """
-    if event in (SessionEvent.CREATE, SessionEvent.REFRESH):
-        save_session(session.export())
-
-
-def init_client() -> Client:
+def init_client(db_session: Session) -> Client:
     """Connect to bluesky using saved credentials.
 
     Returns:
         Client: bluesky client
     """
     client = Client()
-    client.on_session_change(on_session_change)
+    # client.on_session_change(on_session_change)
 
-    session_string = get_session(client)
+    session_string = get_session(client, db_session)
     client.login(session_string=session_string)
 
     return client
+
+
+if __name__ == "__main__":
+    init_client(init_db_session())
