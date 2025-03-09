2cd80c5ab49e57cd455da2dbef53078a9d774f2d
Scott Davis <sfdavis0@gmail.com>
2025-03-08 23:32:46 -0500

    save creds to db

--- post/login.py
+++ post/login.py
@@ -4,13 +4,13 @@ import getpass
 from typing import Optional
 
 from atproto import Client
-from sqlalchemy import select
+from sqlalchemy import insert, select, update
 from sqlalchemy.orm import Session
 from db.create_db import init_db_session
 from db.models import Credentials
 
 
-def get_session(client: Client, db_session: Session, login_type: str | None = None) -> Optional[str]:
+def get_session(client: Client, db_session: Session, login_type: str | None = "dev") -> Optional[str]:
     """Get session text files if they exist, otherwise prompt for username and
     password.
 
@@ -19,25 +19,60 @@ def get_session(client: Client, db_session: Session, login_type: str | None = No
     """
     query = select(Credentials).filter(Credentials.type == login_type)
     credentials = db_session.execute(query).first()
-    
-    if not credentials:
+    session_string = credentials[0].session if credentials else None
+
+    if not session_string:
         user = getpass.getpass("user:")
         password = getpass.getpass("password:")
         client.login(user, password)
         session_string = client.export_session_string()
 
-        save_session(session_string)
+        save_session(user, password, session_string, db_session, login_type)
+
     return session_string
 
 
-def save_session(session_string: str):
+def save_session(user: str, password: str, session_string: str, db_session: Session, login_type: str):
     """Export current session to a text file.
 
     Args:
         session_string (str): session information
     """
-    with open("session.txt", "w") as f:
-        f.write(session_string)
+    query = (
+        insert(Credentials)
+        .values(
+            {
+                "username": user,
+                "password": password,
+                "session": session_string,
+                "type": login_type,
+            }
+        )
+    )
+    db_session.execute(query)
+    db_session.commit()
+
+
+def update_creds(user: str, password: str, session_string: str, db_session: Session, login_type: str):
+    """Export current session to a text file.
+
+    Args:
+        session_string (str): session information
+    """
+    query = (
+        update(Credentials)
+        .where(Credentials.type == login_type)
+        .values(
+            {
+                "username": user,
+                "password": password,
+                "session": session_string,
+                "type": login_type,
+            }
+        )
+    )
+    db_session.execute(query)
+    db_session.commit()
 
 
 def init_client(db_session: Session) -> Client:
