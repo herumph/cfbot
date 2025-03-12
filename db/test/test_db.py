3c94c24740cc1571e217588e261c6a292379db5a
Scott Davis <sfdavis0@gmail.com>
2025-03-11 22:47:15 -0400

    create db table test

---
+++ db/test/test_db.py
@@ -0,0 +1,24 @@
+from sqlalchemy import inspect
+import pytest
+
+from db.create_db import init_db_session, init_db_engine
+from db.models import Base
+
+@pytest.fixture(scope='session')
+def db_session():
+    return init_db_session()
+
+
+@pytest.fixture(scope='session')
+def db_engine():
+    return init_db_engine("database.db")
+
+
+# test for database table names
+def test_table_names(db_engine):
+    """Test for table names"""
+    Base.metadata.create_all(db_engine)
+    expected_tables = {'games', 'credentials', 'posts'}
+    actual_tables = inspect(db_engine).get_table_names()
+
+    assert expected_tables == set(actual_tables)
