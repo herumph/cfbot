80846602f6d03a3d949400b82211ab903f23f055
Scott Davis <sfdavis0@gmail.com>
2025-03-11 22:47:08 -0400

    split engine creation

--- db/create_db.py
+++ db/create_db.py
@@ -1,14 +1,19 @@
 """Create sqlite db and all tables."""
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import Session
 
 from db.models import Base
 
 
-def init_db_session() -> Session:
+def init_db_engine(database_name: str) -> Engine:
+    """Initialize database engine"""
+    return create_engine(f"sqlite:///{database_name}")
+
+
+def init_db_session(database_name: str | None = "database.db") -> Session:
     """Initialize connection to the sqlite database."""
-    engine = create_engine("sqlite:///database.db")
+    engine = init_db_engine(database_name)
     Base.metadata.create_all(engine)
 
     return Session(engine)
