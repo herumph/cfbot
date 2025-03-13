1d19a42d41fc6583772d4de781c304794653407e
Scott Davis <sfdavis0@gmail.com>
2025-03-12 22:55:46 -0400

    update post_important_plays inputs

--- post/post_important_plays.py
+++ post/post_important_plays.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime, timezone
 
+from atproto import Client
 from sqlalchemy import select, update
 from sqlalchemy.orm import Session
 
@@ -13,7 +14,7 @@ from post.post_game_headers import get_current_games
 from query.common import ESPN_GAME, call_espn
 
 
-def _update_database(session: Session, result: dict[str, str]):
+def _update_database(db_session: Session, result: dict[str, str]):
     """Update database with last post created and if a game is over.
 
     Args:
@@ -33,11 +34,12 @@ def _update_database(session: Session, result: dict[str, str]):
             }
         )
     )
-    session.execute(query)
-    session.commit()
+
+    db_session.execute(query)
+    db_session.commit()
 
 
-def _get_previous_posts(session: Session, last_post_id: str) -> dict[str, str]:
+def _get_previous_posts(db_session: Session, last_post_id: str) -> dict[str, str]:
     """Get information about previous post for a game.
 
     Args:
@@ -48,7 +50,7 @@ def _get_previous_posts(session: Session, last_post_id: str) -> dict[str, str]:
         dict: post information
     """
     query = select(Post).filter(Post.id == last_post_id)
-    last_post = session.execute(query).first()
+    last_post = db_session.execute(query).first()
 
     root_id = last_post[0].root_id if last_post[0].root_id else last_post[0].id
     return {
@@ -112,38 +114,35 @@ def format_scoring_play(drive: dict[str, str]) -> str:
     return play_text + drive_text + score_text
 
 
-def post_important_results(important_results: dict[str, str]):
+def post_important_results(important_results: dict[str, str], db_session: Session, client: Client):
     """Post scoring plays for a game.
 
     Args:
         important_results (dict): information about the drive's scoring play
     """
-    session = init_db_session()
-    client = init_client(session)
-
     for result in important_results:
         # get information about this game from game table
         query = select(Game).filter(Game.id == result["game_id"])
-        game_info = session.execute(query).first()[0]
+        game_info = db_session.execute(query).first()[0]
         assert game_info.last_post_id, "No previous post made for this game"
         result["home"] = game_info.home_team
         result["away"] = game_info.away_team
         result["scoring_team"] = game_info.home_team if game_info.home_team_id == result["scoring_team"] else game_info.away_team
 
         # get parent and root posts from post table
-        previous_post = _get_previous_posts(session, game_info.last_post_id)
+        previous_post = _get_previous_posts(db_session, game_info.last_post_id)
 
         # format post and send it if the score has gone up
         if result["home_score"] > game_info.home_score or result["away_score"] > game_info.away_score:
             previous_post = {k: v for k, v in previous_post.items() if k in ("parent", "root")}
             post_text = format_scoring_play(result)
-            result["last_post_id"] = create_post(client, session, post_text, previous_post)
+            result["last_post_id"] = create_post(client, db_session, post_text, previous_post)
 
             # update database with new information
-            _update_database(session, result)
+            _update_database(db_session, result)
 
 
-def post_about_game(game_id: str):
+def post_about_game(game_id: str, db_session: Session, client: Client):
     """Create bluesky posts about scoring plays for a game.
 
     Args:
@@ -153,13 +152,13 @@ def post_about_game(game_id: str):
     game_info = call_espn(ESPN_GAME + game_id)
     important_results = get_important_results(game_info)
 
-    post_important_results(important_results)
+    post_important_results(important_results, db_session, client)
 
 
-def post_important_plays(date: datetime):
+def post_important_plays(date: datetime, db_session: Session, client: Client):
     """
     TODO: docstring
     """
-    games = get_current_games(date)
+    games = get_current_games(date, db_session)
     for game in games:
-        post_about_game(game["id"])
+        post_about_game(game["id"], db_session, client)
