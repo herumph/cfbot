e7f7db0600e2a212274a138638c71b232e65414b
Scott Davis <sfdavis0@gmail.com>
2025-03-12 20:51:56 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+# what does this actually need to do?
+# 1 - get a days games --> how often does this update and how do we update the data in the database? new table?
+# 2 - post a days games if needed --> how is this post specific tracked in the database? ^use a new table for this with a post id column
+# 3 - post game headers
+# 4 - post important plays
