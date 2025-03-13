82c1ee36c971f20d23cb32fc5d5454a4ebff8826
Scott Davis <sfdavis0@gmail.com>
2025-03-12 21:52:18 -0400

    rename wrapper

--- post_about_cfb.py
+++ post_about_cfb.py
@@ -3,3 +3,11 @@
 # 2 - post a days games if needed --> how is this post specific tracked in the database? ^use a new table for this with a post id column
 # 3 - post game headers
 # 4 - post important plays
+from datetime import datetime, timezone
+
+def main(date: datetime):
+    date = date.strftime("%Y%m%d")
+
+
+if __name__ == "__main__":
+    main(datetime.now(timezone.utc))
