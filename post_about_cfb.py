7f68d15aff1805a46a9687c90d6295ab9edd7f21
Scott Davis <sfdavis0@gmail.com>
2025-03-12 22:31:48 -0400

    update docstring

--- post_about_cfb.py
+++ post_about_cfb.py
@@ -1,8 +1,6 @@
-# what does this actually need to do?
-# 1 - get a days games --> how often does this update and how do we update the data in the database? new table?
-# 2 - post a days games if needed --> how is this post specific tracked in the database? ^use a new table for this with a post id column
-# 3 - post game headers
-# 4 - post important plays
+"""
+Wrapper to call each module needed to get and post about CFB data.
+"""
 from datetime import datetime, timezone
 
 from post.post_game_headers import post_about_current_games
