07f41fc5405ea31990ae766a85893e176e9fa804
Scott Davis <sfdavis0@gmail.com>
2025-09-25 22:08:11 -0400

    fix typo

--- db/models.py
+++ db/models.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
-    """Defintion of base model."""
+    """Definition of base model."""
 
     pass
 
