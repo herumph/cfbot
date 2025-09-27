a68bafbce62bcb9869c5812c2671904ad5d34d41
Scott Davis <sfdavis0@gmail.com>
2025-09-27 19:31:37 -0400

    change return

--- query/get_games.py
+++ query/get_games.py
@@ -5,19 +5,15 @@ from query.query_api import query_scoreboard
 from query.parse_results import parse_games
 
 
-def get_games(date: datetime, group: str | None = "80") -> list[dict]:
+def get_games(date: datetime, group: str | None = "80") -> None:
     """Gathers games from espn and logs them to the database.
 
     Args:
         date (datetime): date to get games for
         group (str | None): ESPN group to query. 80 == FBS, 81 == FCS. Default value is 80.
-
-    Returns:
-        list[dict]: list of each game for a given date
     """
     date = date.strftime("%Y%m%d")
     game_data = query_scoreboard(date, group)
     games = parse_games(game_data)
 
     insert_rows("games", games)
-    return games
